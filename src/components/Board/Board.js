b68d56d3db5a968709658d4273c841b32978b0da
zhangqing <heyzqt@qq.com>
2020-03-30 08:17:13 +0800

    判断胜者

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -23,12 +23,21 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+        return;
+    }
     squares[i] = this.state.xIsNext ? "X": "0";
     this.setState({ squares: squares, xIsNext: !this.state.xIsNext });
   }
 
   render() {
-    const status = "Next player: " + (this.state.xIsNext ? 'X' : '0');
+    const winner = calculateWinner(this.state.squares)
+    let status
+    if (winner) {
+        status = 'Winner is: ' + winner
+    } else {
+        status = "Next player: " + (this.state.xIsNext ? 'X' : '0');
+    }
     return (
       <div>
         <div className="status">{status}</div>
@@ -51,5 +60,25 @@ class Board extends React.Component {
     );
   }
 }
+
+function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
 
 export default Board;
