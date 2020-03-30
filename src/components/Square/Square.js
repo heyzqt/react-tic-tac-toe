a552cc1dcddcad159785f5174ce0f875bd9b824f
zhangqing <heyzqt@qq.com>
2020-03-30 08:06:43 +0800

    改写Square为函数组件

--- src/components/Square/Square.js
+++ src/components/Square/Square.js
@@ -1,15 +1,14 @@
-import React from "react";
+import React from 'react';
 import "./Square.css";
 
 // 井字棋的方格
-class Square extends React.Component {
-  render() {
+// 组件 1.只有1个render方法 2.不包含state 此时就可以将组件写成函数组件
+function Square(props){
     return (
-      <button className="square" onClick={() => this.props.onClick()}>
-        {this.props.value}
+      <button className="square" onClick={() => props.onClick()}>
+        {props.value}
       </button>
     );
-  }
 }
 
 export default Square;
